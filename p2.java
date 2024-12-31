fe262b3ba1a4d2b26ac16de02093a4fbbda9b0d2
boopathiBM <boopathibm19.com>
2024-12-31 13:53:50 +0530

    sample

---
+++ p2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class p2{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the name  of the student: ");
+        String name = scanner.next();
+        System.out.println("name  of the student: "+ name);
+        System.out.println("Enter the age  of the student: ");
+        int age = scanner.nextInt();
+        System.out.println("age of the student: "+ age);
+
+
+        
+
+        
+        // String name = "abc";
+        // int age = 29;
+        // int Celcius =  10;
+        // // int f = (9 * Celcius / 5) + 32;
+        // System.out.println("age  of the student: "+ age);
+
+    }
+}
